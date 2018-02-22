3e95e5f15977da035165c373153776819d361dd6
TorreJohnson <torre.johnson@gmail.com>
2018-02-22 14:06:15 -0500

    completed Classes Travel lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+class Driver {
 
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    let currentYear = new Date(`${year}, 01`);
+    return Math.ceil(((currentYear - this.startDate)/(60*60*24*1000))/365);
+  }
+
+}
+
+class Route {
+
+
+
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    let blocks = 0;
+    blocks += Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+    blocks += Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+    return blocks;
+
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+
+}
